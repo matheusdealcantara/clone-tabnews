333aff9259c49d65f87c5a6c863e0a3fa86d8c0c
Matheus de Alcântara <107211702+matheusdealcantara@users.noreply.github.com>
2025-03-19 21:38:17 +0000

    chore: configura o prettier com scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Implementação do https://www.tabnews.com.br para o https://curso.dev do Felipe Deschamps 
+
+Implementação do https://www.tabnews.com.br para o https://curso.dev do Felipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-function Home () {
-    return <h1>Muito feliz de seguir o curso.dev, testando o deploy do vercel</h1>
+function Home() {
+  return (
+    <h1>Muito feliz de seguir o curso.dev, testando o deploy do vercel</h1>
+  );
+}
+
+function teste() {
+  return;
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
