6e0946aebe15901fac7b9913fad788a540cdd7c6
Matheus de Alcântara <107211702+matheusdealcantara@users.noreply.github.com>
2025-08-28 03:21:02 +0000

    feat: implement session creation

--- infra/errors.js
+++ infra/errors.js
@@ -43,8 +43,29 @@ export class NotFoundError extends Error {
       cause,
     });
     this.name = "NotFoundError";
-    this.action = action;
+    this.action =
+      action || "Verifique se os dados enviados na consulta estão corretos.";
     this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class UnauthorizedError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Usuário não autenticado.", {
+      cause,
+    });
+    this.name = "UnauthorizedError";
+    this.action = action || "Faça novamente o login para continuar.";
+    this.statusCode = 401;
   }
 
   toJSON() {
